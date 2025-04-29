1a7355bed90e96fd34e30b3851b205cadac90cf0
obaidabrmo <obidabrmo89@gmail.com>
2025-04-30 01:07:00 +0300

    Initialize Database with demo data Database backup added to the "backup folder" Start simple backend files with first sql connection

---
+++ backend/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA_Compatible" content="IE=edge">
+    <meta name="viewport " content="width=device-width ,inital-scale=1.0">
+    <title>Home</title>
+
+    <!-- Css File-->
+    <link rel="stylesheet" href="../dist/css/style.min.css">
+    <link rel="stylesheet"
+        href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+
+
+</head>
+
+<body>
+
+    <!-- Header Section strart-->
+    <header class="header">
+        <section class="flex">
+            <div class='bx bx-menu' id="menu-btn"></div>
+            <a href="Home.html" class="logo"> JobSearch </a>
+
+            <nav class="navbar">
+                <a href="Home.html">Home</a>
+                <a href="About.html">About Us</a>
+                <a href="Bot.html">Bot</a>
+                <a href="Admin.html">Admin</a>
+
+            </nav>
+            <a href="Login.html" class="btn" style="margin-top:0% ;">Login</a>
+        </section>
+    </header>
